5bc508172f043fbe8eb088e1d33bf325a57d5355
JuliusQian <jq1539531711@gmail.com>
2025-12-08 23:55:36 +0800

    完善基于session的验证码登录

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,30 @@
+package com.hmdp.utils;
+
+import com.hmdp.dto.UserDTO;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        Object user = session.getAttribute("user");
+        //判断用户是否存在
+        if (user == null){
+            //不存在,拦截
+            response.setStatus(401);
+            return false;
+        }
+        //若存在 保存用户信息到threadlocal
+        UserHolder.saveUser((UserDTO) user);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+}
