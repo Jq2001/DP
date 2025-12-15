7e81fe689c9de405d725ef0c5df76dce3cb3b908
JuliusQian <jq1539531711@gmail.com>
2025-12-15 18:11:28 +0800

    实现基于redis短信登录

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
@@ -8,24 +11,35 @@ import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
  * 服务实现类
  * </p>
- *
  */
 @Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     /**
      * 发送手机验证码
      */
@@ -37,45 +51,58 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         }
         //生成验证码
         String code = RandomUtil.randomNumbers(6);
-        //保存验证码到session
-        session.setAttribute("code",code);
+        //保存验证码到redis
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY + phone, code,LOGIN_CODE_TTL, TimeUnit.MINUTES);
         //发送验证码
-        log.info("发送验证码成功:,{}",code);
+        log.info("发送验证码成功:,{}", code);
 
         return Result.ok();
     }
 
     @Override
     public Result login(LoginFormDTO loginForm, HttpSession session) {
+        String phone = loginForm.getPhone();
         //校验手机号
-        if (RegexUtils.isPhoneInvalid(loginForm.getPhone())) {
+        if (RegexUtils.isPhoneInvalid(phone)) {
             return Result.fail("手机号格式错误!");
         }
-        //校验验证码
-        Object cacheCode = session.getAttribute("code");
+        //从redis获取验证码并且进行校验验证码
+        String cacheCode = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY+ phone);
         String code = loginForm.getCode();
-        if (cacheCode == null||!cacheCode.toString().equals(code)){
+        if (cacheCode == null || !cacheCode.equals(code)) {
             //不一致报错
-            return  Result.fail("验证码错误!");
+            return Result.fail("验证码错误!");
         }
         //一致,根据手机号查询用户
-        User user = query().eq("phone", loginForm.getPhone()).one();
+        User user = query().eq("phone", phone).one();
         //用户不存在,创建新用户保存
         if (user == null) {
-            user = createUserWithPhone(loginForm.getPhone());
+            user = createUserWithPhone(phone);
         }
-        //保存用户信息到session
+        //保存用户信息到redis
+        //随机生成token作为登录令牌
+        String token = UUID.randomUUID().toString(true);
+        //User对象转换为HashMap存储
         UserDTO userDTO = new UserDTO();
         BeanUtils.copyProperties(user, userDTO);
-        session.setAttribute("user",userDTO);
-        return Result.ok();
+        Map<String, Object> userMap = BeanUtil.beanToMap(userDTO,new HashMap<>(),
+                CopyOptions.create()
+                        .setIgnoreNullValue(true)
+                        .setFieldValueEditor((key,value)->value.toString()));
+        //存储
+        String tokenKey = LOGIN_USER_KEY+token;
+        stringRedisTemplate.opsForHash().putAll(tokenKey,userMap);
+        //设置有效期
+        stringRedisTemplate.expire(tokenKey,LOGIN_USER_TTL,TimeUnit.SECONDS);
+        //返回token
+        return Result.ok(token);
     }
 
     private User createUserWithPhone(String phone) {
         //创建用户
         User user = new User();
         user.setPhone(phone);
-        user.setNickName(USER_NICK_NAME_PREFIX+RandomUtil.randomNumbers(6));
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomNumbers(6));
         //保存用户
         save(user);
         return user;
