fcafd2bf6db90b4ae608b48b69efa345bbe08a80
JuliusQian <jq1539531711@gmail.com>
2026-01-06 17:08:05 +0800

    全局唯一ID解决方法之:Redis自增ID

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,45 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class RedisIdWorker {
+    //开始的时间戳
+    private static final long BEGIN_TIMESTAMP = 1767225600L;
+    //序列号位数
+    private static final int COUNT_BITS = 32;
+    public StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public long nextId(String keyPrefix){
+        //生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timeStamp = nowSecond - BEGIN_TIMESTAMP;
+        //生成序列号
+        //获取当前日期(天)
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        //自增长
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+        //拼接后返回
+        return  timeStamp << COUNT_BITS | count;
+    }
+
+    /**
+     * 获取开始时间戳
+     * @param args
+     */
+//    public static void main(String[] args) {
+//        LocalDateTime time = LocalDateTime.of(2026, 1, 1, 0, 0, 0);
+//        long second = time.toEpochSecond(ZoneOffset.UTC);
+//        System.out.println(second);
+//    }
+}
